8d633ced7153f757a53efb3b17d9324937aacde2
Jayyu231 <jayyu231@gmail.com>
2021-09-07 00:30:29 +0800

    Update main.py

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -8,8 +8,6 @@ CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
 
 def get_all_video_in_channel(channel_id):
 
-    # api_key = 'AIzaSyBPBP0LVIiQHpQiSwLC-yWx395RHlK9Y2w'
-
     base_video_url = 'https://www.youtube.com/watch?v='
     base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
 
